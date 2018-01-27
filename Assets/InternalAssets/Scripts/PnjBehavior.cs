83233c17eb6d84a8d68e1aec87a734163b6b3895
Unknown <jeremy.othenin@laposte.net>
2018-01-27 01:43:27 +0100

    IA PNJ + Auto stash before merge of "master" and "origin/master"

---
+++ Assets/InternalAssets/Scripts/PnjBehavior.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum StateMovementMachine {Idle, Interact, Walk};
+
+public class PnjBehavior : MonoBehaviour {
+
+    public int speed = 1;
+    float timer = 0;
+    StateMovementMachine state;
+    Vector3 direction;
+    bool backPos;
+    Vector3 spawnPoint;
+
+    void Start () {
+        state = StateMovementMachine.Idle;
+        spawnPoint = transform.position;
+    }
+	
+	
+	void Update () {
+        
+
+        switch (state) {
+
+            case StateMovementMachine.Idle:
+                timer += Time.deltaTime;
+                if (timer > 5f) {
+                    state = StateMovementMachine.Walk;
+                    direction = Random.insideUnitSphere * 10;
+                    direction.Normalize();
+                }
+                break;
+
+            case StateMovementMachine.Interact:
+
+                break;
+
+            case StateMovementMachine.Walk:
+                Move(backPos);
+                timer -= Time.deltaTime*3;
+                if (timer < 0) {
+                    timer = 0;
+                    backPos = !backPos;
+                    state = StateMovementMachine.Idle;
+                }
+                
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+
+    void Move(bool backToSpawn){
+        if (backToSpawn) {
+            direction = spawnPoint - transform.position;
+            direction.Normalize();
+        }
+        transform.LookAt(direction);
+        transform.position = transform.position + new Vector3(direction.x, 0, direction.z) * Time.deltaTime;
+    }
+}
