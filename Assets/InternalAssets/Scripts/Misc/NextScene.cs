2451b5c1704fe5394b33812cf68e509f99aba69e
Unknown <jeremy.othenin@laposte.net>
2018-02-04 15:43:53 +0100

    Title screen version

---
+++ Assets/InternalAssets/Scripts/Misc/NextScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+
+    SceneManager scene;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("LoadNextScene", 9f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadNextScene() {
+
+        SceneManager.LoadScene(1);
+
+    }
+}
